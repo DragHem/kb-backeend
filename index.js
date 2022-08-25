0fcb9bdb4a26205bdc5e4292da4ae911f3471600
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-08-25 10:20:56 +0200

    (Change) Session settings

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: true,
         saveUninitialized: true,
-        cookie: { secure: false, sameSite: 'none' },
+        cookie: { secure: true, sameSite: 'none' },
     })
 );
 app.use(cookieParser(process.env.SESSION_SECRET));
